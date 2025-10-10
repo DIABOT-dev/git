4c8cd3e91ecd6b154f2db9a4d8082d8d4149614f
DIABOT-dev <tranthomfood@gmail.com>
2025-10-10 14:22:15 +0700

    Update 101_views.sql

--- migrations/101_views.sql
+++ migrations/101_views.sql
@@ -1,16 +1,22 @@
 BEGIN;
+
+-- View tuần
 CREATE VIEW IF NOT EXISTS v_metrics_week AS
-SELECT profile_id,
-       date_trunc('week', ts)::date AS week_start,
-       AVG(value) AS avg_bg,
-       COUNT(*) AS logs_count
+SELECT
+  profile_id,
+  date_trunc('week', ts)::date AS week_start,
+  AVG(value) AS avg_bg,
+  COUNT(*) AS logs_count
 FROM bg_logs
 GROUP BY profile_id, date_trunc('week', ts)::date;
 
+-- View tháng
 CREATE VIEW IF NOT EXISTS v_metrics_month AS
-SELECT profile_id,
-       date_trunc('month', ts)::date AS month_start,
-       COUNT(*) AS logs_count
+SELECT
+  profile_id,
+  date_trunc('month', ts)::date AS month_start,
+  COUNT(*) AS logs_count
 FROM bg_logs
 GROUP BY profile_id, date_trunc('month', ts)::date;
+
 COMMIT;

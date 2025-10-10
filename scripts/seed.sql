9ed7062b1a84bd3cf3bcaa538e3c0f95716a3446
DIABOT-dev <tranthomfood@gmail.com>
2025-10-10 11:11:01 +0700

    Create seed.sql

---
+++ scripts/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO profiles (id, user_id, name, dob, gender, height_cm, chronic_notes, created_at)
+VALUES ('9c913921-9fc6-41cc-a45f-ea05a0f34f2a', '00000000-0000-0000-0000-000000000000',
+        'QA User', '1990-01-01', 'male', 170, 'seed profile', now());
